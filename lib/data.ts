fff72028603b582cfbfa78665da2edf47e6be1eb
--replace-all <avbalano@unionbankph.com>
2024-05-20 16:37:13 +0800

    initial commit

---
+++ lib/data.ts
@@ -0,0 +1,96 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import pragImg from "@/public/prag.png";
+import ubpImg from "@/public/ubp.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Pragmanila Website",
+    description:
+      "We are developing a new website for our company to enhance our online presence and improve customer engagement.",
+    tags: ["React", "Next.js", "Wordpress", "Bootstrap", "Bot Messenger"],
+    imageUrl: pragImg,
+  },
+  {
+    title: "FSC (UnionBank of the Philippines)",
+    description:
+      "We are developing a webpage for UnionBank's product to provide clients with essential information and easy application access.",
+    tags: ["React", "Redux", "Material UI", "Boostrap"],
+    imageUrl: ubpImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "JBOSS",
+  "Tailwind",
+  "Bootstrap",
+  "MongoDB",
+  "Redux",
+  "Wordpress",
+  "Express",
+  "Framer Motion",
+] as const;
